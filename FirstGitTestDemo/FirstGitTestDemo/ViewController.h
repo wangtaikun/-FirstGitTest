55b59cbb452c31052d31e796f5228eb32c11b1a7
wangtaikun <wangtaikun@admindeMacBook-Pro.local>
2018-07-17 16:36:13 +0800

    firstcommit

---
+++ FirstGitTestDemo/FirstGitTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstGitTestDemo
+//
+//  Created by wangtaikun on 2018/7/17.
+//  Copyright © 2018年 wangtaikun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstGitTestDemo/FirstGitTestDemo/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  FirstGitTestDemo
+//
+//  Created by wangtaikun on 2018/7/17.
+//  Copyright © 2018年 wangtaikun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *testing=@"wangtaikun is so cute";
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
